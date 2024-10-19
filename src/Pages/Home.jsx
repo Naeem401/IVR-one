f2065632b00638b46188eabff2232e04cc45bdde
Md Naeem Mia <mdnaeemm401@gmail.com>
2024-10-19 03:39:34 +0300

    Add Catagory Item Image Slider on Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../componant/Home/Banner";
+import ItameSlider from "../componant/Home/ItameSlider";
 import SectionTitle from "../componant/SectionTitle";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             subHeading={"From 12.30pm to 1.00am"}
             heading={"Order Online"}
             ></SectionTitle>
+            <ItameSlider/>
         </div>
     );
 };
